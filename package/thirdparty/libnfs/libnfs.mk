118dca928a60472d64c52a278218b242b259cd14
Daniel Burr <dburr@topcon.com>
2014-02-27 04:56:28 +0100

    [aml] Bump libnfs to version 1.9.2

--- package/thirdparty/libnfs/libnfs.mk
+++ package/thirdparty/libnfs/libnfs.mk
@@ -3,7 +3,7 @@
 # libnfs
 #
 #############################################################
-LIBNFS_VERSION = libnfs-1.3.0
+LIBNFS_VERSION = libnfs-1.9.2
 LIBNFS_SITE = git://github.com/sahlberg/libnfs.git
 LIBNFS_INSTALL_STAGING = YES
 LIBNFS_INSTALL_TARGET = YES
