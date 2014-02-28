d176240af251d55e327a106d0e95ebc4168355f0
Daniel Burr <dburr@topcon.com>
2014-02-28 05:53:30 +0100

    [aml] Revert to kernel to 2.6.34.7
    The newer kernels seem to have a couple of problems:
    * Files are sometimes lost on SD card after a reboot
    * Error message in dmesg: "BUG: Your driver calls ioremap() on system memory.  This leads to architecturally unpredictable behaviour on ARMv6+, and ioremap() will fail in the next kernel release.  Please fix your driver."

--- package/thirdparty/lirc/lirc.mk
+++ package/thirdparty/lirc/lirc.mk
@@ -1,4 +1,4 @@
-LIRC_VERSION = 0.9.0
+LIRC_VERSION = 0.8.7
 LIRC_SOURCE = lirc-$(LIRC_VERSION).tar.gz
 LIRC_SITE = http://downloads.sourceforge.net/project/lirc/LIRC/$(LIRC_VERSION)
 LIRC_INSTALL_STAGING = YES
@@ -7,7 +7,8 @@ LIRC_DEPENDENCIES = linux
 LIRC_MAKE=$(MAKE1)
 
 LIRC_CONF_OPT += --with-kerneldir=$(LINUX_DIR)
-LIRC_CONF_OPT += --with-driver=none
+LIRC_CONF_OPT += --with-driver=all
+LIRC_CONF_OPT += --with-moduledir="/lib/modules/$(LINUX_VERSION_PROBED)/misc"
 
 # hack to avoid mknod (requires root). This will be populated automatically.
 LIRC_CONF_OPT += ac_cv_path_mknod=$(shell which echo)
@@ -28,10 +29,15 @@ define LIRC_DEPMOD
 $(HOST_DIR)/sbin/depmod -b $(TARGET_DIR) -a $(LINUX_VERSION_PROBED)
 endef
 
+define LIRC_REMOVE_BROKEN_DRIVERS
+sed -i 's/lirc_wpc8769l//' $(@D)/drivers/Makefile
+endef
+
 define LIRC_INSTALL_ETC
   cp -rf package/thirdparty/lirc/etc $(TARGET_DIR)
 endef
 
+LIRC_POST_CONFIGURE_HOOKS += LIRC_REMOVE_BROKEN_DRIVERS
 LIRC_POST_INSTALL_TARGET_HOOKS += LIRC_DEPMOD
 LIRC_POST_INSTALL_TARGET_HOOKS += LIRC_INSTALL_ETC
 
